8f8dd78d08b53e17dc55b8e0b513aac37965af3d
Shashank singh <shashank.singh@binmile.com>
2024-07-30 11:24:19 +0530

    auth

--- src/Navigation/AppNavigator.tsx
+++ src/Navigation/AppNavigator.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
 import ScreenLogin from '../Screens/ScreenLogin/ScreenLogin';
 import ScreenRegister from '../Screens/ScreenSignup/ScreenSignup';
 import ScreenHome from '../Screens/ScreenHome/ScreenHome';
+import { RootState } from '../utils/redux/rootReducer';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+
   return (
-
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={ScreenLogin} />
-        <Stack.Screen name="Register" component={ScreenRegister} />
+    <Stack.Navigator>
+      {!isAuthenticated ? (
+        <>
+          <Stack.Screen name="Login" component={ScreenLogin} />
+          <Stack.Screen name="Register" component={ScreenRegister} />
+        </>
+      ) : (
         <Stack.Screen name="Home" component={ScreenHome} />
-      </Stack.Navigator>
-
+      )}
+    </Stack.Navigator>
   );
 };
 
