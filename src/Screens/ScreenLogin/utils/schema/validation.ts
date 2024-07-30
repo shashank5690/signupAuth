dc4950a57b6b3a9c849b4e7fb66e3ebb29f74c45
Shashank singh <shashank.singh@binmile.com>
2024-07-30 11:33:08 +0530

    validations

--- src/Screens/ScreenLogin/utils/schema/validation.ts
+++ src/Screens/ScreenLogin/utils/schema/validation.ts
@@ -1,6 +1,15 @@
 import * as yup from 'yup';
 
 export const schema = yup.object().shape({
-    username: yup.string().required('Username is required'),
-    password: yup.string().required('Password is required'),
-  });
+  username: yup.string()
+    .min(4, 'Username must be at least 4 characters long')
+    .max(20, 'Username must be no more than 20 characters long')
+    .required('Username is required'),
+
+  password: yup.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
+    .matches(/\d/, 'Password must contain at least one number')
+    .matches(/[!@#$%^&*()_+{}\[\]:;"'<>,.?~\\/-]/, 'Password must contain at least one special character')
+    .required('Password is required'),
+});
--- src/Screens/ScreenSignup/utils/schema/validation.ts
+++ src/Screens/ScreenSignup/utils/schema/validation.ts
@@ -2,8 +2,21 @@ import * as yup from 'yup';
 
 export const schema = yup.object().shape({
   name: yup.string().required('Name is required'),
-  email: yup.string().email('Invalid email format').required('Email is required'),
-  phone: yup.string().matches(/^\d+$/, 'Phone number should be digits only').required('Phone number is required'),
+
+  email: yup.string()
+    .email('Invalid email format')
+    .required('Email is required'),
+
+  phone: yup.string()
+    .matches(/^\d+$/, 'Phone number should be digits only')
+    .required('Phone number is required'),
+
   username: yup.string().required('Username is required'),
-  password: yup.string().required('Password is required'),
+
+  password: yup.string()
+    .min(8, 'Password must be at least 8 characters long')
+    .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
+    .matches(/\d/, 'Password must contain at least one number')
+    .matches(/[!@#$%^&*()_+{}\[\]:;"'<>,.?~\\/-]/, 'Password must contain at least one special character')
+    .required('Password is required'),
 });
